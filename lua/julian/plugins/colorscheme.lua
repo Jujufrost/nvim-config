427928674dab438123e0b8252196ed83300c16f1
Jujufrost <julianf.219@gmail.com>
2024-02-02 21:14:32 -0500

    Fixed colorscheme

--- lua/julian/plugins/colorscheme.lua
+++ lua/julian/plugins/colorscheme.lua
@@ -1,11 +1,10 @@
 return {
   {
-    "Mofiqul/dracula.nvim",
 	"savq/melange-nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme melange]])
+    vim.cmd([[colorscheme melange]])
     end,
   },
   }
