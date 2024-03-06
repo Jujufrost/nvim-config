d8a3dc64f0def419547805a8e2355cd581a0bd56
Jujufrost <julianf.219@gmail.com>
2024-03-06 15:21:49 -0500

    Fixed TS toggle

--- lua/julian/core/keys.lua
+++ lua/julian/core/keys.lua
@@ -3,5 +3,5 @@ local map = vim.api.nvim_set_keymap
 vim.g.mapleader = " "
 
 map('i', 'jk', '<Esc>', {})
-map('n', 'n', [[:NvimTreeToggle<cr>]], {})
+map('n', '<leader>n', [[:NvimTreeToggle<cr>]], {})
 map('n', 'ff', [[:Telescope find_files<cr>]], {})
