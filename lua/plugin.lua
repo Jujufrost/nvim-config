431a0a5f7526d3e0928935d8dd5c0c705556804a
julian <julian@my-pc.tailb5eaad.ts.net>
2026-08-20 20:38:27 -0400

    get treesitter working, update readme

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,14 +1,28 @@
 vim.pack.add({
   'https://github.com/nvim-mini/mini.nvim',
   'https://github.com/neovim/nvim-lspconfig',
-  'https://github.com/nvim-treesitter/nvim-treesitter',
+  {
+    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+    version = 'main',
+  },
   'https://github.com/nvim-tree/nvim-tree.lua',
   'https://github.com/nvim-tree/nvim-web-devicons',
 })
 
-
 vim.cmd.colorscheme('miniwinter')
+
 require('mini.basics').setup()
 require('mini.surround').setup()
 require('nvim-tree').setup()
-vim.lsp.enable({'lua_ls', 'gcc'})
+
+require('nvim-treesitter').install({
+  'lua',
+  'cpp',
+  'c',
+  'python',
+})
+
+vim.lsp.enable({
+  'lua_ls',
+  'clangd',
+})
